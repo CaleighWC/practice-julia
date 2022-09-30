4f93a2e09aa6b84e0adf92d459395c3f352cf974
Caleigh Charlebois <cwc@zoology.ubc.ca>
2022-09-30 13:25:43 -0700

    Get example plot working

---
+++ scripts/plots_example.jl
@@ -0,0 +1,4 @@
+using UnicodePlots
+plt = lineplot([-1, 2, 3, 7], [-1, 2, 9, 4], title = "Example Plot", name = "my line", xlabel = "x", ylabel = "y")
+println(plt)
+
--- scripts/submit.sh
+++ scripts/submit.sh
@@ -4,5 +4,7 @@ module load StdEnv/2020 julia/1.7.0
 
 julia hello.jl
 
-julia install_packages.jl
+# julia install_packages.jl
+
+julia plots_example.jl
 
